2fa72620e2316e0a686441b1308935ad0267bc16
Pavel Susorov <susorovpp@gmail.com>
2020-12-20 21:26:34 +0300

    test 5 done

--- JS_basic/5/game.js
+++ JS_basic/5/game.js
@@ -3,12 +3,6 @@
 const settings = {
     rowsCount: 10,
     colsCount: 10,
-    // startPositionX: 0,
-    // startPositionY: 0,
-    // startDirection: 'right',
-    // stepsInSeconds: 5,
-    // playerCellColor: '#AA3333',
-    // emptyCellColor: '#EEEEEE',
 };
 
 const chess = {
