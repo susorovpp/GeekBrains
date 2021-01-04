576a8809825f3202c9feaede84cc1d974c60260c
Pavel Susorov <susorovpp@gmail.com>
2021-01-04 22:42:45 +0300

    сделал 2 пункт ДЗ

--- JS_basic/5/game.js
+++ JS_basic/5/game.js
@@ -7,6 +7,55 @@ const settings = {
 
 const chess = {
     settings,
+    figures: [
+        {name: 'r', color: 'b', pos: '11'},
+        {name: 'k', color: 'b', pos: '12'},
+        {name: 'b', color: 'b', pos: '13'},
+        {name: 'Q', color: 'b', pos: '14'},
+        {name: 'K', color: 'b', pos: '15'},
+        {name: 'b', color: 'b', pos: '16'},
+        {name: 'k', color: 'b', pos: '17'},
+        {name: 'r', color: 'b', pos: '18'},
+        {name: 'p', color: 'b', pos: '21'},
+        {name: 'p', color: 'b', pos: '22'},
+        {name: 'p', color: 'b', pos: '23'},
+        {name: 'p', color: 'b', pos: '24'},
+        {name: 'p', color: 'b', pos: '25'},
+        {name: 'p', color: 'b', pos: '26'},
+        {name: 'p', color: 'b', pos: '27'},
+        {name: 'p', color: 'b', pos: '28'},
+        {name: 'p', color: 'w', pos: '71'},
+        {name: 'p', color: 'w', pos: '72'},
+        {name: 'p', color: 'w', pos: '73'},
+        {name: 'p', color: 'w', pos: '74'},
+        {name: 'p', color: 'w', pos: '75'},
+        {name: 'p', color: 'w', pos: '76'},
+        {name: 'p', color: 'w', pos: '77'},
+        {name: 'p', color: 'w', pos: '78'},
+        {name: 'r', color: 'w', pos: '81'},
+        {name: 'k', color: 'w', pos: '82'},
+        {name: 'b', color: 'w', pos: '83'},
+        {name: 'Q', color: 'w', pos: '84'},
+        {name: 'K', color: 'w', pos: '85'},
+        {name: 'b', color: 'w', pos: '86'},
+        {name: 'k', color: 'w', pos: '87'},
+        {name: 'r', color: 'w', pos: '88'},
+    ],
+
+    figureHtml: {
+        pw: '&#9817;',
+        bw: '&#9815;',
+        rw: '&#9814;',
+        kw: '&#9816;',
+        Kw: '&#9812;',
+        Qw: '&#9813;',
+        pb: '&#9823;',
+        bb: '&#9821;',
+        rb: '&#9820;',
+        kb: '&#9822;',
+        Kb: '&#9818;',
+        Qb: '&#9819;',
+    },
 
     /**
      * @property {HTMLElement} gameContainerEI контейнер игры (DOM элемент)
@@ -32,6 +81,9 @@ const chess = {
                 // Создаем и добавляем ячейку
                 const td = document.createElement('td');
                 tr.appendChild(td);
+
+                //Присваиваем атрибут для того, чтобы разместить шахматные фигуры
+                td.setAttribute('data-position', `${row}${col}`)
 
                 // Если строка нулевая (первая по счету) или последняя, значит выводим буквы к ней.
                 // Нули из массива с названием колонок не выводим, они нам не нужны
@@ -74,7 +126,28 @@ const chess = {
         
         return false;
     },
+
+    // Метод просто для вывода фигур.
+    renderFigure() {
+        
+        for (let i = 0; i < 32; i += 1) {
+            // Берем одну фигуру
+            const figure = this.figures[i];
+            // Получаем имя фигуры и цвет в одну строку
+            const figureHtmlProperty = figure.name + figure.color;
+            // Получаем код фигуры из this.figureHtml используя строку из названия фигуры и ее цвета
+            const figureCode = this.figureHtml[figureHtmlProperty];
+            //Вставляем в ячейку
+            const figurePosition = figure.pos;
+            //Находим нужную ячейку по координате
+            const td = document.querySelector(`td[data-position="${figurePosition}"]`);
+            //Вставляем соответствующий код фигуры в найденную ячейку
+            td.innerHTML = figureCode;
+        }
+    },
+
 };
 
 //Запускаем метод отображения карты
 chess.renderMap();
+chess.renderFigure();
