a3308d0eb236db38c73d4dbe44b86dadeedac773
susorovpp <susorovpp@gmail.com>
2021-04-03 23:13:01 +0300

    сделал задание 2.4 через создание объектов

--- JS_prof/2/2.4/js.js
+++ JS_prof/2/2.4/js.js
@@ -1,19 +1,37 @@
-document.getElementById('btn1').addEventListener('click', () => {
-    const promise = new Promise((resolve, reject) => {
-        fetch('success.json')
-            .then(result => result.json())
-            .then(data => resolve(data.result))
-    });
+class Button {
+    constructor(url) {
+        this.url = url
+    }
 
-    promise.then(text => alert(text))
-});
+    init() {
+        const promise = new Promise((resolve, reject) => {
+            fetch(this.url)
+                .then(result => result.json())
+                .then(data => resolve(data.result));
+        });
+        
+        promise.then(text => alert(text));
+    }
+}
 
-document.getElementById('btn2').addEventListener('click', () => {
-    const promise = new Promise((resolve, reject) => {
-        fetch('error.json')
-            .then(result => result.json())
-            .then(data => resolve(data.result))
-    });
 
-    promise.then(text => alert(text))
-});
+// document.getElementById('btn1').addEventListener('click', () => {
+//     const promise = new Promise((resolve, reject) => {
+//         fetch('success.json')
+//             .then(result => result.json())
+//             .then(data => resolve(data.result))
+//     });
+
+//     promise.then(text => alert(text))
+// });
+
+// document.getElementById('btn2').addEventListener('click', () => {
+//     const promise = new Promise((resolve, reject) => {
+//         fetch('error.json')
+//             .then(result => result.json())
+//             .then(data => resolve(data.result))
+//     });
+
+//     promise.then(text => alert(text))
+// });
+
