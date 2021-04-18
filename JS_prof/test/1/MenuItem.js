a84318a0f4a0545ff623e07dfe2096db4037c8bc
susorovpp <susorovpp@gmail.com>
2021-04-18 18:59:27 +0300

    Упорядочивание контента

--- JS_prof/test/1/MenuItem.js
+++
@@ -1,9 +0,0 @@
-class MenuItem {
-    constructor(href, title) {
-        this.href = href;
-        this.title = title;
-    }
-    render() {
-        return `<li><a href="${this.href}">${this.title}</a></li>`;
-    }
-}
